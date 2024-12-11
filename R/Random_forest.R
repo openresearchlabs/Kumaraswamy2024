261ec3e8ffb8c059155612a342f0616a64ad3d93
antagomir <leo.lahti@iki.fi>
2024-12-11 23:57:24 +0200

    RF

--- R/Random_forest.R
+++ R/Random_forest.R
@@ -1,9 +1,9 @@
 #Random Forest Implementation in R, Use R version 4.4.2
 #set working directory to the data directory of the files (Session->set working directory)
 ######################################################
-install.packages("randomForest")
-library(randomForest)
 
+#install.packages("randomForest")
+library(randomForest)
 
 #For Random_forest, Log 10 transformation of LC-HRMS metabolite data 
 #and % relative abundance the original genus-level HITChipdata, 
@@ -11,10 +11,22 @@ library(randomForest)
 #(Cluster-P and Cluster B/R or Fermented milk eater (B+D) and control(A+C) 
 #or Fermented Soybean eater (B+C) and control (A+D)).
 
-my_data <- read.csv("../data/MILK.csv", header = TRUE) #import data
+# my_data <- read.csv("../data/MILK.csv", header = TRUE, row.names=1) #import data
+# names(my_data) #column names
 
-names(my_data) #column names
+# Load data
+source("data.R")
 
+# Pick relative abundance data
+my_data <- assay(tse, "relabundance") %>% as.matrix
+
+# Add group info 
+my_data <- as.data.frame(t(my_data))
+my_data$Group <- tse$Group
+colnames(my_data) <- gsub(" ", "_", colnames(my_data))
+colnames(my_data) <- gsub("\\.", "", colnames(my_data))
+
+# Run random forest
 my_ranfor <- randomForest(Group ~ ., importance=TRUE, ntree=10000,
                           proximity=TRUE, data = my_data)#random forest 
 
